65ebf90684e51d6f597fc8924b33133e7f69b74b
kaushik <kontact.kaushik@gmail.com>
2023-06-28 13:27:39 +0530

    initial commit

---
+++ src/main/java/roopya/money/utility/RoopyaMoneyUtilityApplication.java
@@ -0,0 +1,13 @@
+package roopya.money.utility;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoopyaMoneyUtilityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoopyaMoneyUtilityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/roopya/money/utility/RoopyaMoneyUtilityApplicationTests.java
@@ -0,0 +1,13 @@
+package roopya.money.utility;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoopyaMoneyUtilityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
